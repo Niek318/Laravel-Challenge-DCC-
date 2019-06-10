ae7dbd0851b7b679993669248304bc56d81b0fb3
Niek318 <38814969+Niek318@users.noreply.github.com>
2019-06-10 12:39:31 +0200

    Initiated laraval project
    Getting started with the basics of laravel

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title')
+
+  Contact us
+
+@endsection
+
+@section('content')
+
+  <h1>Contact</h1>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+
+
+@section('content')
+  <h1>My first Website!</h1>
+
+@endsection
